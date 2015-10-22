2be38f7a520358eac70a5761b030118e3cb84a27
Dominique Miller <dominique.p.miller@gmail.com>
2015-10-22 19:48:03 -0400

    refactored class to only be the factory

--- class_tip_calc.rb
+++ class_tip_calc.rb
@@ -1,42 +1,51 @@
 class Calculate
   attr_accessor :bill, :percent, :people
-  def initialize
+  def initialize(bill, percent, people)
      @bill = bill
      @percent = percent
      @people = people
   end
 
-  def tip
-    bill * percent
-  end
-
-  def total_bill
-    bill + tip
-  end
-
-  def split
-    bill / people
-  end
 end
 
-calc = Calculate.new
 
-puts "How much is your bill?"
-calc.bill = gets.chomp.to_f
+def start
+  puts "How much is your bill?"
+    bill = gets.chomp.to_f
 
-puts "How many people are paying?"
-calc.people = gets.chomp.to_i
+  puts "What percent tip would you like?"
+  answer = gets.chomp.to_f
+    percent = answer / 100
 
-puts "What percent tip would you like?"
-answer = gets.chomp.to_f
-calc.percent = answer / 100
+  puts "How many people are paying?"
+      people = gets.chomp.to_i
 
+  @calc = Calculate.new(bill, percent, people)
+end
 
-puts "Thank you for dining at Wyncafe!"
-puts "********************************"
-puts "You bill is      $#{calc.bill}"
-puts "The tip is       $#{calc.tip}"
-puts "The final total: $#{calc.total_bill}"
-puts "Split total each $#{calc.split}"
-puts "********************************"
-puts "Thank you, have a nice day!"
+def tip
+  @calc.bill * @calc.percent
+end
+
+def total_bill
+  @calc.bill + tip
+end
+
+def split
+  total_bill / @calc.people
+end
+
+def output
+  puts "Thank you for dining at Wyncafe!"
+  puts "********************************"
+  puts "You bill is      $#{@calc.bill}"
+  puts "The tip is       $#{tip.round(2)}"
+  puts "The final total: $#{total_bill}"
+  puts "Split total each $#{split.round(2)}"
+  puts "********************************"
+  puts "Thank you, have a nice day!"
+end
+
+start
+
+output
